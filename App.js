d621f7608c5ff0512a778cb078eca17a5f1f0121
Kaulin99 <kaueandrade04@hotmail.com>
2025-08-19 22:33:00 -0300

    Tem drop

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
+  
+  const [valor1, setValor1] = useState(0);
+  const [valor2, setValor2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  
+  //Botões de operação 
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: '+', value: 'soma'},
+    { label: '-', value: 'subtracao'},
+    { label: 'X', value: 'vezes'},
+    { label: '/', value: 'divisao'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Calculadora</Text>
+
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        placeholder="Selecione"
+      />
       <StatusBar style="auto" />
     </View>
   );
